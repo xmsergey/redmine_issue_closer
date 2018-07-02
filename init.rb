7c3f0483facc2b45b719b2d025bc562f8808c2d3
Pavel Ishenin <isheninp@gmail.com>
2018-07-02 19:52:45 +0300

    0207

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ Redmine::Plugin.register :issuecloser do
   name 'Issuecloser plugin'
   author 'Pavel Ishenin'
   description 'Close issues plugin'
-  version '0.0.1'
+  version '1.0.0'
   url 'https://github.com/isheninp/redmine_issue_closer'
   author_url 'https://github.com/isheninp'
   settings :default => {
